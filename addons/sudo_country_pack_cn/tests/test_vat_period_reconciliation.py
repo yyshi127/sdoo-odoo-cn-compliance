c595da7cbe89e5132d1c3ab03942d5c0770ec0e6
yyshi127 <yyshi127@users.noreply.github.com>
2026-07-24 02:59:35 +0800

    Fix VAT company-context test user

--- addons/sudo_country_pack_cn/tests/test_vat_period_reconciliation.py
+++ addons/sudo_country_pack_cn/tests/test_vat_period_reconciliation.py
@@ -1936,12 +1936,10 @@ class TestChinaVatPeriodReconciliation(AccountTestInvoicingCommon):
         foreign_company = self.env["res.company"].create(
             {"name": "Foreign VAT execution context"}
         )
-        (self.scope_author | self.reviewer).write(
+        self.reviewer.write(
             {"company_ids": [Command.link(foreign_company.id)]}
         )
-        (self.scope_author | self.reviewer).invalidate_recordset(
-            ["company_ids"]
-        )
+        self.reviewer.invalidate_recordset(["company_ids"])
         scope = self._control_account_scope(
             sources["sale"],
             sources["purchase"],
